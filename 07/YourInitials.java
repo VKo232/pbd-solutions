2257fda8501eddd8dedac9da28346d129725ed33
VKo232 <31933554+VKo232@users.noreply.github.com>
2018-02-14 14:24:52 -0500

    added 07

---
+++ 07/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+    	System.out.println("V           V     K     K ");
+    	System.out.println(" V         V      K   K      ");
+    	System.out.println("  V       V       K  K       ");
+    	System.out.println("   V     V        K K    ");
+    	System.out.println("    V   V         K   K   ");
+    	System.out.println("     V V          K     K  ");
+    	System.out.println("      V           K       K");
+  	
+    }
+}
