79074742c325d12c089c855f0ab0252e079ed776
VKo232 <31933554+VKo232@users.noreply.github.com>
2018-02-14 14:08:56 -0500

    Add files via upload

---
+++ 06/LetterToYourself.java
@@ -0,0 +1,16 @@
+
+public class LetterToYourself {
+	public static void main(String[] args) {
+		System.out.println("+---------------------------------------------------------+\n"
+				+ "|                                                    #### |"
+				+ "\n|                                                    #### |"
+				+ "\n|                                                    #### |"
+				+ "\n|                                                         |"
+				+ "\n|                                                         |"
+				+ "\n|                              victor ko                  |"
+				+ "\n|                              1 Microsoft Way            |"
+				+ "\n|                              Redmond, WA 98104          |"
+				+ "\n|                                                         |" 
+				+ "\n+---------------------------------------------------------+");
+	}
+}
