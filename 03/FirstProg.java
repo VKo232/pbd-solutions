23fc511ab013f105010693b2fd7266d55b259cab
VKo232 <31933554+VKo232@users.noreply.github.com>
2018-02-15 12:26:38 -0500

    Update FirstProg.java

--- 03/FirstProg.java
+++ 03/FirstProg.java
@@ -1,5 +1,5 @@
 public class FirstProg {
-	public static void main(String[] args) {
-		System.out.println("School is cool");
-	}
+    public static void main(String[] args) {
+        System.out.println("School is cool");
+    }
 }
