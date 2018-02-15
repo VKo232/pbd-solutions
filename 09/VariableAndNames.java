a028e8565ae9f65efae7aa81b9d04f30d307a006
VKo232 <31933554+VKo232@users.noreply.github.com>
2018-02-15 13:01:03 -0500

    Update VariableAndNames.java

--- 09/VariableAndNames.java
+++ 09/VariableAndNames.java
@@ -1,32 +1,31 @@
 
 public class VariableAndNames {
-    public static void main( String[] args )
-    {
-        int cars, drivers, passengers, cars_not_driven, cars_driven;
-        double space_in_a_car, carpool_capacity, average_passengers_per_car;
+    public static void main(String[] args){
+        int cars, drivers, passengers, carsNotDriven, carsDriven;
+        double spaceInACar, carpoolCapacity, averagePassengersPerCar;
         // num of cars
         cars = 100;
         // num space in car
-        space_in_a_car = 4; // floating point is a decimal number
+        spaceInACar = 4; // floating point is a decimal number
         // num drivers 
         drivers = 30;
         // num passengers
         passengers = 90;
         // empty cars without drivers
-        cars_not_driven = cars - drivers;
+        carsNotDriven = cars - drivers;
         // cars being drived is num of drivers
-        cars_driven = drivers;
+        carsDriven = drivers;
         // num of spots available
-        carpool_capacity = cars_driven * space_in_a_car;
+        carpoolCapacity = carsDriven * spaceInACar;
         // passengers in each car
-        average_passengers_per_car = passengers / cars_driven;
+        averagePassengersPerCar = passengers / carsDriven;
 
 
-        System.out.println( "There are " + cars + " cars available." );
-        System.out.println( "There are only " + drivers + " drivers available." );
-        System.out.println( "There will be " + cars_not_driven + " empty cars today." );
-        System.out.println( "We can transport " + carpool_capacity + " people today." );
-        System.out.println( "We have " + passengers + " to carpool today." );
-        System.out.println( "We need to put about " + average_passengers_per_car + " in each car." );
+        System.out.println("There are " + cars + " cars available.");
+        System.out.println("There are only " + drivers + " drivers available.");
+        System.out.println("There will be " + carsNotDriven + " empty cars today.");
+        System.out.println("We can transport " + carpoolCapacity + " people today.");
+        System.out.println("We have " + passengers + " to carpool today.");
+        System.out.println("We need to put about " + averagePassengersPerCar + " in each car.");
     }
 }
