dde465faec193b5978363959679284eb11299cbf
VKo232 <31933554+VKo232@users.noreply.github.com>
2018-02-15 12:39:49 -0500

    Update YourInitials.java

--- 07/YourInitials.java
+++ 07/YourInitials.java
@@ -1,12 +1,11 @@
 public class YourInitials {
     public static void main(String[] args) {
-    	System.out.println("V           V     K     K ");
-    	System.out.println(" V         V      K   K      ");
-    	System.out.println("  V       V       K  K       ");
-    	System.out.println("   V     V        K K    ");
-    	System.out.println("    V   V         K   K   ");
-    	System.out.println("     V V          K     K  ");
-    	System.out.println("      V           K       K");
-  	
+        System.out.println("V           V     K     K ");
+        System.out.println(" V         V      K   K      ");
+        System.out.println("  V       V       K  K       ");
+        System.out.println("   V     V        K K    ");
+        System.out.println("    V   V         K   K   ");
+        System.out.println("     V V          K     K  ");
+        System.out.println("      V           K       K");
     }
 }
