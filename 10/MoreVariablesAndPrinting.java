8c022997dddd534d7ed2fb1d61a348e1eb0e0b99
VKo232 <31933554+VKo232@users.noreply.github.com>
2018-02-15 13:07:20 -0500

    Update MoreVariablesAndPrinting.java

--- 10/MoreVariablesAndPrinting.java
+++ 10/MoreVariablesAndPrinting.java
@@ -1,29 +1,27 @@
 public class MoreVariablesAndPrinting {
-    public static void main( String[] args ) {
-        String Name, Eyes, Teeth, Hair;
-        int Age, Height, Weight;
+    public static void main(String[] args) {
+        String name, eyes, teeth, hair;
+        int age, height, weight;
 
-        Name = "Zed A. Shaw";
-        Age = 35;     // not a lie
-        Height = 74;  // inches
-        Weight = 180; // lbs
-        Eyes = "Blue";
-        Teeth = "White";
-        Hair = "Brown";
+        name = "Zed A. Shaw";
+        age = 35;     // not a lie
+        height = 74;  // inches
+        weight = 180; // lbs
+        eyes = "Blue";
+        teeth = "White";
+        hair = "Brown";
 
-        System.out.println( "Let's talk about " + Name + "." );
-        System.out.println( "He's " + Height + " inches tall." );
+        System.out.println( "Let's talk about " + name + "." );
+        System.out.println( "He's " + height + " inches tall." );
         System.out.println( "He's " + Weight + " pounds heavy." );
         System.out.println( "Actually, that's not too heavy." );
-        System.out.println( "He's got " + Eyes + " eyes and " + Hair + " hair." );
-        System.out.println( "His teeth are usually " + Teeth + " depending on the coffee." );
+        System.out.println( "He's got " + eyes + " eyes and " + hair + " hair." );
+        System.out.println( "His teeth are usually " + teeth + " depending on the coffee." );
 
         // This line is tricky; try to get it exactly right.
-        System.out.println( "If I add " + Age + ", " + Height + ", and " + Weight
-            + " I get " + (Age + Height + Weight) + "." );
+        System.out.println( "If I add " + age + ", " + height + ", and " + weight+ " I get " + (age + height + weight) + "." );
         // convert pounds to kg
-        System.out.println("I am " + Weight + "lb. This is equal to " + Weight *0.45359+ "kg" );
-        System.out.println("I am " + Height + "inches. This is equal to " + Height * 2.54 + "cm.");
-        
+        System.out.println("I am " + weight + "lb. This is equal to " + weight * 0.45359 + "kg" );
+        System.out.println("I am " + height + "inches. This is equal to " + height * 2.54 + "cm.");
     }
 }
