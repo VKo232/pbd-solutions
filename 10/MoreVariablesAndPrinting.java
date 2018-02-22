67e9f2f012b7afe36880beffac2825e3ba11a01b
qwertyuiop232 <vk07275@gmail.com>
2018-02-22 16:45:38 -0500

    edited

--- 10/MoreVariablesAndPrinting.java
+++ 10/MoreVariablesAndPrinting.java
@@ -13,7 +13,7 @@ public class MoreVariablesAndPrinting {
 
         System.out.println( "Let's talk about " + name + "." );
         System.out.println( "He's " + height + " inches tall." );
-        System.out.println( "He's " + Weight + " pounds heavy." );
+        System.out.println( "He's " + weight + " pounds heavy." );
         System.out.println( "Actually, that's not too heavy." );
         System.out.println( "He's got " + eyes + " eyes and " + hair + " hair." );
         System.out.println( "His teeth are usually " + teeth + " depending on the coffee." );
