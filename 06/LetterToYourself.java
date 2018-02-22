8a729a141a2345136c379fdf414bfc428197880c
qwertyuiop232 <vk07275@gmail.com>
2018-02-22 16:49:11 -0500

    edited

--- 06/LetterToYourself.java
+++ 06/LetterToYourself.java
@@ -2,7 +2,7 @@ public class LetterToYourself {
     public static void main(String[] args) {
         System.out.println("+---------------------------------------------------------+");
         System.out.println("|                                                    #### |");
-        System.out.println("|                                                    #### |");                                                    #### |"
+        System.out.println("|                                                    #### |");                                                
         System.out.println("|                                                    #### |");
         System.out.println("|                                                         |");
         System.out.println("|                                                         |");
